f2ae3b37794ebaf8aa25050b97b6cadb81c8c742
Kiril Chobanov <62597135+kiril2000@users.noreply.github.com>
2020-04-07 23:45:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NumsDividingBy3And5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
